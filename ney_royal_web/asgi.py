47b566ef46a24eab6893238870269e09ec0e2684
jenadiusnicholaus <ucode888@gmail.com>
2021-11-22 17:33:28 +0300

    second commit

---
+++ ney_royal_web/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ney_royal_web project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ney_royal_web.settings')
+
+application = get_asgi_application()
